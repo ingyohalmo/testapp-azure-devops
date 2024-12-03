4d91819281120f5e28167cc0696e4049f32d9345
Yohalmo Diaz <ydd770@ftr.com>
2024-12-03 09:34:07 -0600

    Continuous Integration CI Pipeline

--- Startup.cs
+++ Startup.cs
@@ -47,6 +47,7 @@ namespace ProjectApi
             });
         }
 
+        // Comment to test CI/CD
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
